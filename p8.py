959db1cecf4da088f8f74d8955b2c55f0a49c985
Prajwalais <prajwalbakaningi7@gmail.com>
2025-10-23 17:20:50 +0530

    salary

--- p8.py
+++ p8.py
@@ -4,3 +4,28 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+def main():
+    salary = float(input("Enter employee's salary: "))
+    bonus_salary = salary * 1.10
+    print(f"Original Salary: {salary}")
+    print(f"Bonus Added: {bonus_salary}")
+
+if __name__ == "__main__":
+    main()
+
+
+
+    def main():
+    salary = float(input("Enter employee's salary: "))
+    bonus_salary = salary * 1.10
+    after_tax = bonus_salary * 0.95  # Deduct 5% tax
+
+    print(f"Original Salary: {salary}")
+    print(f"Bonus Added: {bonus_salary}")
+    print(f"After Tax Deduction: {after_tax}")
+
+if __name__ == "__main__":
+    main()
+
